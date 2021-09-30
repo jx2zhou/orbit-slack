207d837d1c5e35c36b11dad40160bd199eb92d36
Kevin Pack <kapack@ucdavis.edu>
2021-09-30 12:07:23 -0700

    no error message, but image doesn't send

--- SlackbotDemo.py
+++ SlackbotDemo.py
@@ -1,10 +1,11 @@
 from slack import WebClient
 from slack.errors import SlackApiError
+import logging 
 
-
-
-slack_token = "xoxb-2538044939847-2546117567878-1O9yWKZQJxMWoNkLc9tRLJJm"
+slack_token = "xoxb-2538044939847-2546117567878-Q548dhVCnL8I2v3dt4oylnuZ"
 client = WebClient(token=slack_token)
+
+logger = logging.getLogger(__name__)
 
 ###channel Checking
 
@@ -26,14 +27,18 @@ client = WebClient(token=slack_token)
 # except SlackApiError as e:
 #     print(f"Error: {e}")
 
+file_name = './michael_what.jpeg'
+channel_id = 'C02FU1DQSAK'
 try:
-  response = client.chat_postMessage(
-    channel="C02FU1DQSAK",
-    text="Hello from your Slackboss version -1!"
+  response = client.files_upload(
+    channel='hackathon',
+    initial_comment="hey",
+    file=file_name
   )
+  logger.info(response)
 except SlackApiError as e:
   # You will get a SlackApiError if "ok" is False
+  logger.error("error uploading file: {}".format(e))
   assert e.response["error"]  # str like 'invalid_auth', 'channel_not_found'
 
 # bool = client.rtm_connect(with_team_state=False)
-print("test")
