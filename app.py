adef4d7400181da2611b8327b42cac583116c727
Joshua Zhou <jzxjxzh@gmail.com>
2021-10-01 14:46:29 -0700

    Single file app.py; time and settings still needs work

--- app.py
+++ app.py
@@ -2,16 +2,35 @@
 # Flask app so that interactive elements have a URL to call back to.
 # This is using the default dev server and a http tunnel. Jank af
 
+from datetime import datetime
+from datetime import date
 from flask import Flask, request, Response
 import json
 from slack import WebClient
 from slack.errors import SlackApiError
 import time
+import os
+import random
 
 OAUTH_TOKEN = "xoxb-2538044939847-2546117567878-Q548dhVCnL8I2v3dt4oylnuZ"
+USER_TOKEN ="xoxp-2538044939847-2565386880881-2543059939895-826f6a444d8a2a76c9e457bdfbd0f17f"
 
 app = Flask(__name__)
 client = WebClient(token=OAUTH_TOKEN)
+user_client = WebClient(token=USER_TOKEN) 
+
+###Initial Connection to Slackboss
+##Attempts connection
+##Testing connection - terminate program on failed connection (work in progress)
+connected = client.api_test()
+if connected['ok'] == False:
+    print("Sorry, Slackboss forgot to pay the wifi bill and can't connect!")
+    print("Please check your token and try again!")
+    print("Token:", auth_token)
+    quit()
+
+time_to_break, duration = None, None
+
 
 ### BUTTONS
 
@@ -198,10 +217,225 @@ def send_default_active_response(channel_id='U02GATBE7E0'):
     except SlackApiError as e:
       assert e.response["error"]
 
-while True:
+### SLACKBOTDEMO CODE
+
+###Channel Checker: tells you the conversation id of a channel and if it exists
+#input: string, channel you are looking for
+#output: string, conversation ID, returns None if the conversation doesn't exist
+def checkChannel(myChannel):
+    all_channels = client.conversations_list()
+    #check all slack group channels to make sure this one exists
+    for response in all_channels:
+            for channel in response["channels"]:
+                if channel["name"] == myChannel:
+                    print(f"Found conversation ID:",channel["id"])
+                    return channel["id"]
+    print("We couldn't find a group with that name, sorry!")
+    return None
+
+###Message sender: sends message to channel (will be expanded to send to people
+#and multiple groups)
+#input: (string, string, optional strnig). (channel name of destination*or conversationID, text Message, attachment file path)
+#output: None
+def sendText(destinationChannel, message, attachments = None):
+    try:
+      response = client.chat_postMessage(
+        channel=destinationChannel,
+        text=message
+      )
+      print("Message sent to", destinationChannel, "successfully!")
+    except SlackApiError as e:
+      # You will get a SlackApiError if "ok" is False
+      assert e.response["error"]  # str like 'invalid_auth', 'channel_not_found''
+
+##sends single image specified by image object, as well as message to channel of your choosing
+#input: string, string, image object - give function the chanel to speak to and message and imageLink in image objecty
+#output: None
+def sendImage(destinationChannel, message, image = None):
+    try:
+      response = client.chat_postMessage(
+        channel=destinationChannel,
+        text=message,
+        attachments = image
+      )
+      print("Message sent to", destinationChannel, "successfully!")
+    except SlackApiError as e:
+      # You will get a SlackApiError if "ok" is False
+      assert e.response["error"]  # str like 'invalid_auth', 'channel_not_found'
+
+#will pick a random meme from a list of image urls and then sends one to the specified channel
+#input: int. range of memes available
+#output: none. will simply post the image
+def sendRandomMeme(destinationChannel, title, message = "Commencing Meme Blast..."):
+    curr_directory = os.path.dirname(__file__) #<-- this is the computers absolute directory for this foler
+    local_path = "Memes/StopWorkingMemes.txt"#<-- we are combining our current path with the next path to our memes
+
+    #construct the local path from the absolute path
+    memes_file_path = os.path.join(curr_directory, local_path)
+    memes_file = open(memes_file_path, "r")
+    list_of_memes = [(line.strip()).split() for line in memes_file]
+
+    #select a meme randomly
+    selected_meme = random.choice(list_of_memes)[0]
+
+    image = [{"title": title, "image_url": selected_meme}]
+    sendImage(destinationChannel, message, image)
+
+#creates a dictionary calender that stores the break plans for each day
+#only for septamber,october as of now for testing purposes
+###months will have a list of days and each day will have its own list of breaks. each days
+#has its own list because it stores the times of their breaks as well as there duration
+#as follows (break time PST, break duration in mins)
+def createCalender():
+    cal = {"September": None, "October": None}
+    #give the months the correct number of days (a dictionary may improve efficiancy)
+    cal["September"] = [None for x in range(31)]
+    cal["October"] = [None for x in range(32)]
+    #add fake mock schedule for testing:
+    #the pairs have (break time, duration) break times are in hours, so 16.25 *60 would convert it to minutes
+
+    #break object format (float, float): (time of break in minutes, duration of break in minutes)
+    break1 = (12.50 * 60, 5)
+    #this means at 12:30 there will be a 5 min break. Convert hours to minute by multiplying by 60!!!
+    break2 = (14.66 * 60, 30)
+    break3 = (15 * 60, 30)
+    break4 = (16 * 60, 30)
+
+    cal["October"][1] = [break1, break2, break3, break4, (1400 * 60, 5)]#day 30 is position 31. pairs are (break time pst, break duration in mins)
+    return cal
+
+#this mightneed to be done for only one user at a time rather than in a channel
+#link to Slack Api method https://api.slack.com/methods/dnd.setSnooze
+#have this go when it is break time, set time length to the same as the break length
+def setDoNotDisturb(destinationChannel, time):
+    try:
+        message = "Do Not Disturb set for {} minutes".format(time)
+        sendText(destinationChannel, message)
+        response = user_client.dnd_setSnooze(num_minutes=1)
+        print("Began do not disturb time")
+    except SlackApiError as e:
+        print("Failed to set do not disturb time")
+        assert e.response["error"]
+
+def findNextBreak(calender):
+    today = date.today()
+
+    day = today.strftime("%d")
+    month = today.strftime("%B")
+
+    todays_sched = calender[month][int(day)]
+
+    now = datetime.now()
+    #track i to keep index place what breaks have passed and to delete
+    i=1
+    for break_time, break_duration in todays_sched:
+        #convert current time into minutes
+        current_hour_in_minutes = float(now.strftime("%H")) *60
+        current_minute = float(now.strftime("%M"))
+        formated_time_in_minutes = current_hour_in_minutes + current_minute
+
+        #find how many minutes until next break
+        if break_time >= formated_time_in_minutes:
+            #to find how long until next break, subtract first the hours. this is because you need to convert
+            #the time into minutes to get it correct
+            time_until_break_in_minutes = break_time-formated_time_in_minutes
+            # time_until_break_in_minutes = (time_until_break_in_minutes*60) - current_minute
+
+            # time_until_break_in_minutes = (break_time-formated_time_in_minutes)
+
+            calender[month][int(day)] = todays_sched[i:]
+            return (time_until_break_in_minutes, break_duration)
+        i += 1
+    #return none if you can't find anything
+    return (None, None)
+
+#start a timer that ends when your next break starts
+def start_break_timer(id, minutes):
+    sendRandomMeme(id, "Take A Break Now!!!")
+    sendText(id, "Break will end in " + str(minutes))
+    for i in range(minutes):
+        time.sleep(60)
+    sendText(id, "The break is over, back to work!!")
+
+
+id = checkChannel("hackathon")
+setDoNotDisturb(id, 2)
+workday_is_over = False
+calender = createCalender()
+in_break = False
+counter = 0
+time_to_break, duration = findNextBreak(calender)
+break_duration = 0
+
+@app.route("/poll")
+def poll_handler():
+    global time_to_break, in_break, counter, break_duration, workday_is_over, duration
     time.sleep(1)
-    print('x')
+    if time_to_break == None:
+        print("No more breaks today!")
+        sendText(id, "No more breaks today!! Final Stretch!")
 
+    elif in_break:
+        if break_duration % 60 == 0:
+            sendRandomMeme(id, "Take A Break Now!!!")
+            sendText(id, "Break will end in " + str(duration - break_duration / 60))
+            # for i in range(minutes * 60):
+            #     time.sleep(1)
+        break_duration += 1
+        if break_duration >= duration * 60:
+            sendText(id, "The break is over, back to work!!")
+            in_break = False
+            time_to_break, duration = findNextBreak(calender)
+            break_duration = 0
+    else:
+        if counter % (60) == 0:
+            sendText(id, str(time_to_break)+" mins until next break!")
 
+        if time_to_break <= 0:
+            sendRandomMeme(id, "Take A Break Now!!!")
+            sendText(id, "Break will end in " + str(duration))
+            in_break = True    
+        # start_break_timer(id, duration)
+    print(counter, time_to_break, duration, break_duration)
+    time_to_break -= 1.0 / 60
+    counter += 1
+    return ''
+
+#test the functionality
+# def TestFunctions():
+#     while not workday_is_over:
+#         time.sleep(1)
+#         if time_to_break == None:
+#             print("No more breaks today!")
+#             sendText(id, "No more breaks today!! Final Stretch!")
+
+#         elif in_break:
+#             sendRandomMeme(id, "Take A Break Now!!!")
+#             sendText(id, "Break will end in " + str(minutes))
+#             for i in range(minutes * 60):
+#                 time.sleep(1)
+#             sendText(id, "The break is over, back to work!!")
+#             in_break = False
+#             time_to_break, duration = findNextBreak(calender)
+#         else:
+#             if counter % (60 * 10) == 0:
+#                 sendText(id, str(time_to_break)+" mins until next break!")
+
+#             if time_to_break <= 1:
+#                 in_break = True    
+#             # start_break_timer(id, duration)
+#         print(counter)
+#         counter += 1
+
+    # start_timer(1)
+
+    # sendText(id, "This function is working correctly")
+    # image_url = "https://pics.awwmemes.com/need-dis-sleepy-la-paresse-on-twitter-almost-lunch-time-54243381.png"
+    # attachments = [{"title": "Take Your Break!!!", "image_url": image_url}]
+    # sendImage(id, "Commencing Meme Blast...", attachments)
+
+    # sendRandomMeme(id, "Take A Break Now!!!")
+
+# TestFunctions()
 
 
---
+++ timer.sh
@@ -0,0 +1,5 @@
+while true
+do
+	sleep 1
+	wget -q --spider http://8a97-107-3-141-83.ngrok.io/poll &
+done
